74da06f3e59372a27c78ca19b31f191b39fc86f3
Eduardo San Miguel <abx88as@gmail.com>
2022-11-15 22:15:39 -0600

    small fixed and indent settings tweaks

--- AlpineJSConfig.php
+++ AlpineJSConfig.php
@@ -1,25 +1,28 @@
 <?php namespace ProcessWire;
-class AlpineJSConfig extends ModuleConfig {
-		public function getInputfields() {
-				$class = rtrim($this->className(), 'Config');
-				$config = $this->config;
-				$inputfields = $this->wire(new InputfieldWrapper());
-				$f = $this->wire('modules')->get('InputfieldRadios');
-				$f->attr('name', 'version');
-				$f->attr('label', 'Version');
-				$files = $this->files;
-				$versions = $files->find("{$config->paths->$class}alpinejs", [
-					'allowDirs' => true,
-					'recursive' => false,
-					'returnRelative'=> true
-				]);
-				foreach($versions as $i => $version){
-						$version = rtrim($version,"/");
-						$versions[$i] = $version;
-						$f->addOption(rtrim($version,"/"));
-				}
-				$f->attr('value', isset($data['version']) ? $data['version'] : $versions[0]);
-				$inputfields->add($f);
-				return $inputfields;
+
+class AlpineJSConfig extends ModuleConfig
+{
+	public function getInputfields()
+	{
+		$class = rtrim($this->className(), 'Config');
+		$config = $this->config;
+		$inputfields = $this->wire(new InputfieldWrapper());
+		$f = $this->wire('modules')->get('InputfieldRadios');
+		$f->attr('name', 'version');
+		$f->attr('label', 'Version');
+		$files = $this->files;
+		$versions = $files->find("{$config->paths->$class}alpinejs", [
+			'allowDirs'      => true,
+			'recursive'      => false,
+			'returnRelative' => true
+		]);
+		foreach ($versions as $i => $version) {
+			$version = rtrim($version, "/");
+			$versions[$i] = $version;
+			$f->addOption(rtrim($version, "/"));
 		}
+		$f->attr('value', isset($this->version) ? $this->version : $versions[0]);
+		$inputfields->add($f);
+		return $inputfields;
+	}
 }
